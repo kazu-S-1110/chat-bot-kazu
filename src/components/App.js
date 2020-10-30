917931755b114214ff982b81a21324688c2e5850
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-10-31 01:48:37 +0900

    setNameを作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Signin from "./Signin";
 
 export default () => {
+  const [name, setName] = useState("") //useStateの中身に初期値を設定可能
+  console.log({name});
+
   return (
-    <Signin/>
+    <Signin setName={setName}/> //SigninコンポへsetNameの値を渡す
     
   )
-
-
 };
