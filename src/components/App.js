3654c28c9c61cec8c0eb089e1295a7546ad14f38
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-11-01 03:42:05 +0900

    メインコンポーネントの開発準備のため設定ファイルを作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Signin from './Signin';
 import Main from './main';
+import config from '../config.json';
 
 export default () => {
   const [name, setName] = useState(''); //useStateの中身に初期値を設定可能
-  console.log({ name });
 
-  if (name === '') {
+  if (config.signInEnabled && name === '') {
     return <Signin setName={setName} />;
   } else {
     return <Main name={name} />;
