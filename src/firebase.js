1fc78217de4501a7e87babcfb0785fd683b1e264
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2020-11-02 15:14:15 +0900

    FIrebaseの設定を環境変数へ移植

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,25 @@
 import firebase from 'firebase';
 
+const {
+  REACT_APP_FIREBASE_API_KEI,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBtKs6nnwlISWQmWiJpasCtpGhuDYsMDdg',
-  authDomain: 'chat-demo-kazu.firebaseapp.com',
-  databaseURL: 'https://chat-demo-kazu.firebaseio.com',
-  projectId: 'chat-demo-kazu',
-  storageBucket: 'chat-demo-kazu.appspot.com',
-  messagingSenderId: '946060471627',
-  appId: '1:946060471627:web:1e5cd3bc3c912495cb0cce',
+  apiKey: REACT_APP_FIREBASE_API_KEI,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
+
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database(); //これがFirebaseコンソール上のデータベースを参照している
